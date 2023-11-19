4ad1db8c0df5f5d5503beacaa35aee590418016c
Rushabh Aradwad <aradwadrushabh@gmail.com>
2023-11-19 12:05:28 +0530

    The core implementation of the HashMap

--- HashP3Sol.java
+++ HashP3Sol.java
@@ -1,10 +1,4 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 
 // } Driver Code Ends
 //User function Template for Java
--- c1factorial.java
+++ c1factorial.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 //Driver Code Starts
 class factorial
