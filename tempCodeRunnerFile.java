82a2839cc6be5944d11860ee890b2822ab97853e
Rushabh Aradwad <aradwadrushabh@gmail.com>
2023-11-23 22:21:11 +0530

    String Problem 2 Java Solution

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,5 +1,19 @@
-for(Map.Entry(String,Integer) e : HM.entrySet()){
-            System.out.println(HM.getKey)();
-            System.out.println(HM.getValue());
+class Solution
+{
+    //Function to count the number of substrings that start and end with 1.
+    public static int binarySubstring(int a, String str)
+    {
+        int count = 0;
+        int onesCount = 0;
 
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '1') {
+                onesCount++;
+            }
         }
+
+        count = onesCount * (onesCount - 1) / 2;
+        return count;
+        // Your code here
+    }
+}
